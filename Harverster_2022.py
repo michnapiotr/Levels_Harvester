07a11e69a53e66126e255b5b70ac8e95598dd7e3
piotrmichna <piotrmichna@gmail.com>
2022-08-26 07:13:45 +0200

    Harvester 25.08.2022 one file a day

--- Harverster_2022.py
+++ Harverster_2022.py
@@ -6,19 +6,30 @@ import time
 url = 'https://danepubliczne.imgw.pl/api/data/hydro'
 
 response = requests.get(url)
-
-
 json_resp = response.json()
 
-now = datetime.now().strftime("%d.%m.%Y %H.%M.%S")
-print(now, ' response: ', response.status_code)
+#starting_time = datetime(2022, 8, 26)
+now = datetime.now().strftime("%d.%m.%Y")
+
+
+def get_file_name():
+    file_name = '25.08.2022.csv'
+    if file_name != now:
+        file_name = str(now) + '.csv'
+        return file_name
+    else:
+        return 'Error'
+
+
+
 
 while True:
 
+    print(now, ' response: ', response.status_code)
 
-    with open('rivers_levels {}.csv'.format(now), 'a', encoding='utf-8', errors='ignore', newline='') as file:
+    with open(get_file_name(), 'a', encoding='utf-8', errors='ignore', newline='') as file:
         river_writer = csv.writer(file)
-        river_writer.writerow(['Date','River', 'Station', 'Level'])
+        river_writer.writerow(['Date', 'River', 'Station', 'Level'])
 
         for each in json_resp:
             river = each['rzeka']
@@ -29,5 +40,5 @@ while True:
 
         file.close()
 
-    time.sleep(3600)
+    time.sleep(10)
 
